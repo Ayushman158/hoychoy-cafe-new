49ab1e6db97c7a5b01dbbc27637b69fca4f26653
Ayushman Bharadwaj <ayushmanbharadwaj@Ayushmans-MacBook-Air.local>
2025-12-10 23:02:18 +0530

    Update OWNER_PHONE to +918638864806 for WhatsApp order notifications

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-export const OWNER_PHONE = "917002400184";
+export const OWNER_PHONE = "918638864806";
 export const UPI_ID = "ayushman15899-4@okaxis";
 export const MERCHANT_NAME = "HoyChoy Café";
 export const BACKEND_URL = "https://hoychoy-cafe-backend.onrender.com";
